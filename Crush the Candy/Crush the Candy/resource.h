513de19f86d5acc4af1f9720bb6e86dd70afa0ba
Emmamelon10 <131699411+Emmamelon10@users.noreply.github.com>
2023-06-21 07:09:34 -0300

    que crearamos un juego en grupo B)

---
+++ Crush the Candy/Crush the Candy/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Crush the Candy.rc
+//
+#define Ventana                         101
+#define Tablero                         104
+#define Candy_b1	                    105
+#define Tablero_P                       1002
+#define casilla0_0                      1003
+#define casilla0_1                      1004
+#define casilla0_2                      1005
+#define casilla0_3                      1006
+#define casilla0_4                      1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
